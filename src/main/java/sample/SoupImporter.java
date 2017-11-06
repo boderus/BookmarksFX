e5c3ceedb15adcaee35d32dbd0905b12970225d0
boderus <luki.scala@gmail.com>
2017-11-06 23:25:08 +0100

    test

--- src/main/java/sample/SoupImporter.java
+++ src/main/java/sample/SoupImporter.java
@@ -32,6 +32,8 @@ public class SoupImporter implements BookmarksImporter {
 
         directories.push("default");
 
+        System.out.println("Git Test");
+
 
 /*
         for (Element e : doc.getElementsByTag("DL"))
